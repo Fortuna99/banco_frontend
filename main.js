f9f60208906776e53e369b03caa4be0f36dca9b9
Sergio <you@example.com>
2022-06-10 19:46:22 +0200

    Primer commit

---
+++ main.js
@@ -0,0 +1,50 @@
+const gestor1 = {
+    id: 1,
+    usuario: 'gestor1',
+    password: 'gestor1',
+    correo: 'gestor1@gmail.com'
+};
+
+const gestor2 = {
+    id: 2,
+    usuario: 'gestor2',
+    password: 'gestor2',
+    correo: 'gestor2@gmail.com'
+};
+
+const gestor3 = {
+    id: 3,
+    usuario: 'gestor3',
+    password: 'gestor3',
+    correo: 'gestor3@gmail.com'
+};
+
+const cliente1 = {
+    id: 1,
+    id_gestor: 1,
+    usuario: 'cliente1',
+    password: 'cliente1',
+    correo: 'cliente1@gmail.com',
+    saldo: 3500
+};
+
+const cliente2 = {
+    id: 2,
+    id_gestor: 2,
+    usuario: 'cliente2',
+    password: 'cliente2',
+    correo: 'cliente2@gmail.com',
+    saldo: 3000
+    
+};
+
+const cliente3 = {
+    id: 3,
+    id_gestor: 3,
+    usuario: 'cliente3',
+    password: 'cliente3',
+    correo: 'cliente3@gmail.com',
+    saldo: 33000
+    
+};
+
