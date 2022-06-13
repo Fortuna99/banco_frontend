9c4f1ab95aad793e6d7dd12251a32e266e903328
Sergio <you@example.com>
2022-06-13 19:13:43 +0200

    copia de seguridad

--- main.js
+++ main.js
@@ -48,3 +48,34 @@ const cliente3 = {
     
 };
 
+/*
+Escribe un programa que almacene los objetos creados en el ejercio 
+anterior del proyecto dentro de un array (un array por cada modelo de datos). 
+A continuación, recorre cada uno de los arrays y muestra todas propiedades..
+*/
+
+const gestores = [gestor1, gestor2, gestor3];
+
+const clientes = [cliente1, cliente2, cliente3];
+
+// gestores es un array
+// gestor es un objeto con las propiedades: id, usuario, password, correo
+console.log('--- GESTORES ---');
+for (const gestor of gestores) {
+    console.log(`id: ${gestor.id}`);
+    console.log(`usuario: ${gestor.usuario}`);
+    console.log(`password: ${gestor.password}`);
+    console.log(`correo: ${gestor.correo}`);
+    console.log('-----');
+}
+
+console.log('--- CLIENTES ---');
+for (const cliente of clientes) {
+    console.log(`id: ${cliente.id}`);
+    console.log(`usuario: ${cliente.usuario}`);
+    console.log(`password: ${cliente.password}`);
+    console.log(`correo: ${cliente.correo}`);
+    console.log('-----');
+}
+
+
